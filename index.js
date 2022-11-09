dc8cf66acd5d92a407dbb56623c5aecd3c5776e0
Prithiraj Roy <prithirajkumar8519@gmail.com>
2022-11-10 03:45:36 +0600

    abaro add koram

--- index.js
+++ index.js
@@ -9,7 +9,52 @@ function addPlayer( button , name){
 
     
 }
-const numberPlyer = playerList.children.length ;
+
+
+
+function countPlyer(){
+      const budget =  document.getElementById('Budgetplayer');
+        const budgetNumber = parseFloat(budget.value);
+        const numberPlyer = playerList.children.length ;
+        const  playerExpenses = budgetNumber * numberPlyer;
+     
+      const playerExpect = document.getElementById('playerExpenses');
+      const playerExpectAmount =playerExpect.innerText= playerExpenses;
+      budget.value= '';
+    //   const playerexpen = playerExpect.innerText;
+    //   console.log(playerexpen);
+
+    
+   
+
+
+}
+
+function calculateTotal (){
+
+
+    const manager = document.getElementById('manager');
+    const managerNumber = parseFloat(manager.value)
+
+    const coach = document.getElementById('coach');
+    const coachNumber = parseFloat(coach.value)
+
+    const netAmount =  managerNumber + coachNumber  ;
+
+    const totalAmount = document.getElementById('totalAmount');
+    totalAmount.innerText = netAmount  ;
+    manager.value= "";
+    coach.value = "";
+
+
+   
+
+
+
+
+   
+
+}
 
 
 
